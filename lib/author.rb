2f686445c70d3d9e3bde8a248748337a6872a7be
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-21 01:41:33 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+
+attr_accessor :name
+
+def initialize(name)
+  @name = name
+  @posts = []
+end
+
+def posts
+    @posts << name
+end
+
+def add_post(title)
+  @posts << name
+end
+
+
+def posts
+   @posts
+ end
+
+
+end
